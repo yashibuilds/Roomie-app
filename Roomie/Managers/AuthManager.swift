5adccd8852a353e1edf39e207d34de411c17d84a
yashibuilds <yashibuilds@gmail.com>
2026-01-04 02:06:56 -0500

    Save Firebase user as User object in signUp

--- Roomie/Managers/AuthManager.swift
+++ Roomie/Managers/AuthManager.swift
@@ -7,7 +7,16 @@ class AuthManager: ObservableObject {
     @Published var isAuthenticated: Bool = false // true if someone is logged in
     private let db = Firestore.firestore()
     
-    // tell Firebase to create a new account with the given email and password. completion handler tells us if it succeeded or failed after signUp returns.
+    /* signUp:
+    1. Ask Firebase Auth to create a new user with the given email and password
+    2. Check if there was an error from Firebase. If so, stop and return the failure.
+    3. Get the user object from Firebase (UID and email).
+     
+     
+     */
+    
+    
+    
     func signUp(email: String, password: String, name: String, completion: @escaping (Result<User, Error>) -> Void) {
         Auth.auth().createUser(withEmail: email, password: password) { (authResult: AuthDataResult?, error: Error?) in
             // check for error
@@ -17,7 +26,16 @@ class AuthManager: ObservableObject {
             }
             
             // get the user from Firebase
-            
+            if let firebaseUser = authResult?.user {
+                let userID = firebaseUser.uid
+                
+                // create the User object
+                let user = User(id: userID, email: email, name: name, deviceToken: nil, roomID: nil, createdAt: Date())
+                
+                // convert User object to dictionary
+                user.toDictionary()
+            }
+        
         }
     }
 }
